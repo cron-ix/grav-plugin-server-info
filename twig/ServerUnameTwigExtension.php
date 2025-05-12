74c71b7be78bc515d17db7167db07dd9d5b6cf41
cron-ix <74101164+cron-ix@users.noreply.github.com>
2025-05-12 17:05:12 +0200

    add serverCPU function

--- twig/ServerUnameTwigExtension.php
+++ twig/ServerUnameTwigExtension.php
@@ -11,6 +11,7 @@ class ServerUnameTwigExtension extends \Twig_Extension
 		return [
 			new \Twig_SimpleFunction('serverUname', [$this, 'serverUnameFunction']),
                         new \Twig_SimpleFunction('serverMachine', [$this, 'serverMachineFunction']),
+			new \Twig_SimpleFunction('serverCPU', [$this, 'serverCPUFunction']),
 		];
 	}
 	public function serverUnameFunction($arg)
@@ -27,6 +28,12 @@ class ServerUnameTwigExtension extends \Twig_Extension
 	public function serverMachineFunction()
         {
                 $command = 'echo "$(cat /sys/devices/virtual/dmi/id/sys_vendor) $(cat /sys/devices/virtual/dmi/id/product_version)"';
+                passthru($command, $output);
+                return $output;
+        }
+	public function serverCPUFunction()
+        {
+                $command = "cat /proc/cpuinfo | grep 'model name' | uniq | awk -F ':' '{print $2}'";
                 passthru($command, $output);
                 return $output;
         }
