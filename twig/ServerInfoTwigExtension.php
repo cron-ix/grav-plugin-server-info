43f6e5b1768ec51a88d79235419ee29d3c2d8b0d
cron-ix <74101164+cron-ix@users.noreply.github.com>
2025-05-12 17:53:03 +0200

    Update ServerInfoTwigExtension.php

--- twig/ServerInfoTwigExtension.php
+++ twig/ServerInfoTwigExtension.php
@@ -13,6 +13,7 @@ class ServerInfoTwigExtension extends \Twig_Extension
                         new \Twig_SimpleFunction('serverMachine', [$this, 'serverMachineFunction']),
 			new \Twig_SimpleFunction('serverCPU', [$this, 'serverCPUFunction']),
 			new \Twig_SimpleFunction('serverMemTotal', [$this, 'serverMemTotalFunction']),
+			new \Twig_SimpleFunction('serverCpuTemp', [$this, 'serverCpuTempFunction']),
 		];
 	}
 	public function serverUnameFunction($arg)
@@ -43,5 +44,31 @@ class ServerInfoTwigExtension extends \Twig_Extension
                 $command = "cat /proc/meminfo | grep MemTotal | awk -F ':' '{print $2}' | xargs";
                 passthru($command, $output);
                 return $output;
+        }
+        public function serverCpuTempFunction()
+        {
+                // Raspberry Pi
+                // $sensor_path = '/sys/class/thermal/thermal_zone0/temp';
+                // ThinkCentre
+                $sensor_path = '/sys/class/thermal/thermal_zone8/temp';
+
+                // read content
+                $sensor_value = @file_get_contents($sensor_path);
+                // error handling
+                // file not found or NULL
+                if ($sensor_value === FALSE) {
+                                $return_value = sprintf('Temperature sensor not found at "%1$s", see "README.md" (%2$s) for further information.', $sensor_path, $readme_url);
+                }
+                // return value is > 0
+                // ToDo: can core temp be lower than 0 (extreme cooling scenarios)?
+                elseif (intval("$sensor_value") > 0) {
+                                $return_value = number_format($sensor_value/1000, 1, ',', '.') . " °C";
+                }
+                // value is not numeric or 0 and lower
+                else {
+                                $return_value = sprintf('No temperature found at "%1$s".', $sensor_path);
+                }
+
+                return $return_value;
         }
 }
